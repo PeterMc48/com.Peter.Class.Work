96f120c5b3ee9e097f0582cca6952e7b0173e5f9
Peter A. Mc Cafferty <t00095451@365s.ittralee.ie>
2018-09-13 12:51:12 +0100

    udate 12.50 13/09/18

--- src/ProblemSet0Exercise4.java
+++ src/ProblemSet0Exercise4.java
@@ -3,28 +3,37 @@ import javax.swing.*;
 public class ProblemSet0Exercise4 {
     public static void main(String[] args) {
 
-        int number,i=0,totalNum=0;
+        int number=0,i=0,totalNum=0;
         String num;
         num = JOptionPane.showInputDialog("Please enter a number");
 
-        while(!num.equals("q")){
-
-            for(int j=1;j<num.length();j++)
+        while(!num.equals("q")) {
+            if (num == null)
             {
-                if(!Character.isDigit(j))
+                if (num.charAt(0) == '-' && num.length() == 1)
                 {
-                number = Integer.parseInt(num);
-                i++;
+                    for (int j = 1; j < num.length(); j++)
+                    {
+                        char n = num.charAt(j);
+                        if (n < '0' || n > '9')
+                        {
+                            num = JOptionPane.showInputDialog("Please enter a prositive or negative number");
 
-                totalNum +=number;
+                        }
+                        else
+
+                            number = Integer.parseInt(num);
+                            i++;
+                            totalNum +=number;
+                    }
                 }
-                else
-                num = JOptionPane.showInputDialog("Please enter a prositive or negative number");
+            }
+
             }
 
             num = JOptionPane.showInputDialog("Please enter a number");
 
-        }
+
 
             JOptionPane.showMessageDialog(null, "you entered a number this many times: "+i+
                                                                          "\nThe total of all the numbers entered is: "+totalNum);
